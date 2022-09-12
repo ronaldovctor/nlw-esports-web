9cd516e042c851d55737a93ecddf28f88db4028c
Ronaldo <ronaldovctor@outlook.com>
2022-09-12 20:43:20 -0300

    First elements and adding phosphor-react

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['.src/**/*.tsx'],
+	content: ['./src/**/*.tsx', './index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {},
+			backgroundImage: {
+				galaxy: 'url("./src/assets/bg-galaxy.png")',
+				'nlw-gradient':
+					'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 45.94%, #E1D55D 95.57%)',
+				'game-gradient':
+					'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);',
+			},
+		},
 	},
 	plugins: [],
 }
